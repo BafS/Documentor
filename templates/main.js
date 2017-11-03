64e322c027c3bd9d863e57ed02f03f30919f817f
BafS <fabacrans@gmail.com>
2017-11-03 14:57:19 +0100

    Add search box to highlight words in the doc

--- templates/main.js
+++ templates/main.js
@@ -45,6 +45,93 @@ const toggleClass = (element, className) => {
   }
 };
 
+
+/**
+ * Highlight the given word
+ * @param {Element} root
+ * @param {string} word
+ * @param {string} [className='highlight'] className
+ */
+function highlightWord(root, word, className = 'highlight') {
+  const excludeElements = ['script', 'style', 'iframe', 'canvas'];
+  let found = false;
+
+  /**
+   * @returns {Node[]}
+   */
+  const textNodesUnder = () => {
+    const walk = document.createTreeWalker(root, NodeFilter.SHOW_TEXT, null, false);
+    const text = [];
+
+    while (walk.nextNode()) {
+      if (excludeElements.indexOf(walk.currentNode.parentElement.tagName.toLowerCase()) < 0) {
+        text.push(walk.currentNode);
+      }
+    }
+    return text;
+  };
+
+  /**
+   * Highlight words
+   * @param {Node} n
+   * @returns {boolean}
+   */
+  const highlightWords = (n) => {
+    const indexOfNode = (node, i) => node.nodeValue.toLowerCase().indexOf(word.toLowerCase(), i);
+    let after;
+    let span;
+    let i = indexOfNode(n);
+
+    if (!found && i > -1) {
+      found = true;
+    }
+
+    while (i > -1) {
+      after = n.splitText(i + word.length);
+      span = document.createElement('span');
+      span.className = className;
+      span.appendChild(n.splitText(i));
+      after.parentNode.insertBefore(span, after);
+      n = after;
+      i = indexOfNode(after, i);
+    }
+  };
+
+  textNodesUnder().forEach(highlightWords);
+  return found;
+}
+
+/**
+ * Remove highlight from elements
+ * @param {Element[]} elements
+ */
+const removeHighlight = (elements) => {
+  elements.forEach((el) => {
+    const prev = el.previousSibling;
+    const next = el.nextSibling;
+    prev.textContent += (el.textContent + next.textContent);
+    el.parentNode.removeChild(next);
+    el.parentNode.removeChild(el);
+  });
+};
+
+const sidebarLinks = document.querySelectorAll('.sidebar ul li a');
+
+/**
+ * Highlight sidebar link
+ * @param {number} index
+ */
+const highlightSidebarIndex = (index) => {
+  sidebarLinks[index].classList.add('highlight');
+};
+
+/**
+ * Remove highlights from sidebar
+ */
+const removeHighlightSidebar = () => {
+  sidebarLinks.forEach(link => link.classList.remove('highlight'));
+};
+
 const main = document.querySelector('.main');
 document.querySelector('button.toggle-sidebar').onclick = () => {
   toggleClass(main, 'full-width');
@@ -65,6 +152,32 @@ document.querySelector('button.previous-page').onclick = () => {
 };
 
 document.querySelector('.tools').classList.remove('hide');
+
+const searchInputEl = document.querySelector('.search-input');
+searchInputEl.classList.remove('hide');
+
+const els = document.querySelectorAll('.page');
+
+searchInputEl.addEventListener('keypress', (e) => {
+  const key = e.which || e.keyCode;
+  if (key === 13) {
+    removeHighlight([...document.querySelectorAll('.content .highlight')]);
+    removeHighlightSidebar();
+    els.forEach((el, pIndex) => {
+      if (highlightWord(el, searchInputEl.value)) {
+        highlightSidebarIndex(pIndex);
+      }
+    });
+  }
+});
+
+searchInputEl.addEventListener('keyup', (e) => {
+  const key = e.which || e.keyCode;
+  if (key !== 13 && searchInputEl.value.length < 1) {
+    removeHighlight([...document.querySelectorAll('.content .highlight')]);
+    removeHighlightSidebar();
+  }
+});
 
 // Listen for hash change
 window.onhashchange = () => {
