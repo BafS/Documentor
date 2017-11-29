46e8a3050f1c53d36e87ed273d194631a0aa6c6c
BafS <fabacrans@gmail.com>
2017-11-29 20:22:43 +0100

    Add `documentor init`

--- bin/documentor.js
+++ bin/documentor.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const yaml = require('js-yaml');
 const yargs = require('yargs');
 const Documentor = require('../src');
+const Init = require('./init');
 
 console.time('time generation');
 
@@ -13,6 +14,7 @@ const { argv } = yargs
   .example('$0 docs -c conf.yml', 'Output html to STDOUT from "docs" folder and read the configuration file "conf.yml"')
   .example('$0 docs -o out.html -w', 'Watch the "docs" folder and regenerate "out.html" on change')
   .example('$0 docs -o out.html --var.name "My Project"', 'Generate "out.html" with custom variables')
+  .example('$0 init', 'Interactive way to initialize your documentation')
   .alias('i', 'input')
   .nargs('i', 1)
   .describe('i', 'Input folder (optional flag)')
@@ -44,43 +46,48 @@ if (!argv.input) {
   process.exit();
 }
 
-const confFile = argv.config || `${argv.input}/_config.yml`;
-let config = {};
-
-if (fs.existsSync(confFile)) {
-  try {
-    config = yaml.safeLoad(fs.readFileSync(confFile, 'utf8'));
-  } catch (e) {
-    console.error(e);
-  }
-} else if (argv.config) {
-  console.log(`Configuration file "${confFile}" does not exists`);
-}
-
-if (argv.var) {
-  // Override the config with user variables
-  config = {
-    ...config,
-    ...argv.var,
-  };
-}
-
-const documentor = new Documentor(argv.input, config);
-
-if (argv.watch) {
-  console.log(`Watch files in '${argv.input}'`);
-  documentor.watch(argv.output, (type, pathname) => {
-    if (pathname) {
-      console.log(`[${type}] ${pathname}`);
-    } else {
-      console.log(type);
-    }
-  });
+if (argv.input === 'init') {
+  Init();
 } else {
-  const res = documentor.generate(argv.output);
-  if (argv.verbose) {
-    res.then(() => {
-      console.timeEnd('time generation');
+  const confFile = argv.config || `${argv.input}/_config.yml`;
+  let config = {};
+
+  if (fs.existsSync(confFile)) {
+    try {
+      config = yaml.safeLoad(fs.readFileSync(confFile, 'utf8'));
+    } catch (e) {
+      console.error(e);
+    }
+  } else if (argv.config) {
+    console.log(`Configuration file "${confFile}" does not exists`);
+  }
+
+  if (argv.var) {
+    // Override the config with user variables
+    config = {
+      ...config,
+      ...argv.var,
+    };
+  }
+
+  const documentor = new Documentor(argv.input, config);
+
+  if (argv.watch) {
+    console.log(`Watch files in '${argv.input}'`);
+    documentor.watch(argv.output, (type, pathname) => {
+      if (pathname) {
+        console.log(`[${type}] ${pathname}`);
+      } else {
+        console.log(type);
+      }
     });
+  } else {
+    const res = documentor.generate(argv.output);
+    if (argv.verbose) {
+      res.then(() => {
+        console.timeEnd('time generation');
+      });
+    }
   }
 }
+
---
+++ bin/init.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const yaml = require('js-yaml');
+const inquirer = require('inquirer');
+
+const questions = [
+  {
+    type: 'input',
+    name: 'name',
+    message: 'Name of your documentation',
+  },
+  {
+    type: 'input',
+    name: 'version',
+    message: 'Documentation version',
+    default: '1.0.0',
+  },
+];
+
+const contentFirstFile = `## Welcome to your brand new documentation
+
+To render this documentation (in index.html) run
+
+\`\`\`bash
+documentator ./{dir} -o index.html
+\`\`\`
+`;
+
+module.exports = () => {
+  inquirer.prompt(questions)
+    .then((ans) => {
+      const dirName = ans.name.replace(' ', '_');
+
+      if (fs.existsSync(dirName)) {
+        throw new Error(`Directory '${dirName}' already exists`);
+      }
+      fs.mkdirSync(dirName);
+
+      fs.writeFileSync(`${dirName}/_config.yml`, yaml.safeDump(ans));
+      fs.writeFileSync(`${dirName}/Introduction.md`, contentFirstFile.replace('{dir}', dirName));
+
+      console.log(`> You can now run 'documentor ./${dirName} -o index.html' to render your documentation.`);
+    });
+};
