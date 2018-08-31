b3137dc0c4b90e9822e7a22abdfa9c21184c89ae
BafS <fabacrans@gmail.com>
2018-09-01 00:56:30 +0900

    Switch to ava

--- test/helpers.js
+++ test/helpers.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+import test from 'ava';
 
 const {
   removeLeadingNumber,
@@ -8,55 +8,49 @@ const {
   getExtension,
 } = require('../src/helpers');
 
-describe('Test helpers', () => {
-  describe('test removeLeadingNumber function', () => {
-    it('should remove the leading number', () => {
-      assert.strictEqual(removeLeadingNumber('0_ThisIsATest'), 'ThisIsATest');
-      assert.strictEqual(removeLeadingNumber('000_ThisIsATest'), 'ThisIsATest');
-      assert.strictEqual(removeLeadingNumber('12345_FAQ'), 'FAQ');
-      assert.strictEqual(removeLeadingNumber('09870_FAQ'), 'FAQ');
-      assert.strictEqual(removeLeadingNumber('-1_FAQ'), '-1_FAQ');
-      assert.strictEqual(removeLeadingNumber('_FAQ'), '_FAQ');
-      assert.strictEqual(removeLeadingNumber('ThisIsATest'), 'ThisIsATest');
-    });
-  });
+test('test removeLeadingNumber function', (t) => {
+  // it should remove the leading number
+  t.is(removeLeadingNumber('0_ThisIsATest'), 'ThisIsATest');
+  t.is(removeLeadingNumber('000_ThisIsATest'), 'ThisIsATest');
+  t.is(removeLeadingNumber('12345_FAQ'), 'FAQ');
+  t.is(removeLeadingNumber('09870_FAQ'), 'FAQ');
+  t.is(removeLeadingNumber('-1_FAQ'), '-1_FAQ');
+  t.is(removeLeadingNumber('_FAQ'), '_FAQ');
+  t.is(removeLeadingNumber('ThisIsATest'), 'ThisIsATest');
+});
 
-  describe('test humanizesSlug function', () => {
-    it('should humanize slug', () => {
-      // assert.strictEqual(humanizesSlug('0_ThisIsATest'), 'This Is A Test');
-      assert.strictEqual(humanizesSlug('12345_FAQ'), 'FAQ');
-      assert.strictEqual(humanizesSlug('-1_FAQ'), '-1 FAQ');
-      assert.strictEqual(humanizesSlug('This_is_a_test'), 'This is a test');
-      assert.strictEqual(humanizesSlug('1_FAQ'), 'FAQ');
-      assert.strictEqual(humanizesSlug('_FAQ'), 'FAQ');
-      assert.strictEqual(humanizesSlug('FAQ'), 'FAQ');
-      // assert.strictEqual(humanizesSlug('0_ThisIsATest'), 'This Is A Test');
-    });
-  });
 
-  describe('test strToSlug function', () => {
-    it('should transform a string to slug slug', () => {
-      // assert.strictEqual(strToSlug('This is a test'), 'Thissatest');
-      assert.strictEqual(strToSlug('A Test'), 'ATest');
-      assert.strictEqual(strToSlug('A  Test'), 'ATest');
-      // assert.strictEqual(strToSlug('A_super Test'), 'AsuperTest');
-    });
-  });
+test('test humanizesSlug function', (t) => {
+  // it should humanize slug
+  // t.is(humanizesSlug('0_ThisIsATest'), 'This Is A Test');
+  t.is(humanizesSlug('12345_FAQ'), 'FAQ');
+  t.is(humanizesSlug('-1_FAQ'), '-1 FAQ');
+  t.is(humanizesSlug('This_is_a_test'), 'This is a test');
+  t.is(humanizesSlug('1_FAQ'), 'FAQ');
+  t.is(humanizesSlug('_FAQ'), 'FAQ');
+  t.is(humanizesSlug('FAQ'), 'FAQ');
+  // t.is(humanizesSlug('0_ThisIsATest'), 'This Is A Test');
+});
 
-  describe('test getBasename function', () => {
-    it('should get the getBasename', () => {
-      assert.strictEqual(getBasename('document.txt'), 'document');
-      assert.strictEqual(getBasename('document.test.md'), 'document.test');
-    });
-  });
+test('test strToSlug function', (t) => {
+  // it should transform a string to slug slug
+  // t.is(strToSlug('This is a test'), 'Thissatest');
+  t.is(strToSlug('A Test'), 'ATest');
+  t.is(strToSlug('A  Test'), 'ATest');
+  // t.is(strToSlug('A_super Test'), 'AsuperTest');
+});
 
-  describe('test getExtension function', () => {
-    it('should get the extension', () => {
-      assert.strictEqual(getExtension('document.txt'), 'txt');
-      assert.strictEqual(getExtension('document.test.txt'), 'txt');
-      assert.strictEqual(getExtension('.txt'), '');
-      assert.strictEqual(getExtension('qwe'), '');
-      assert.strictEqual(getExtension(''), '');
-    });
-  });
+test('test getBasename function', (t) => {
+  // it should get the getBasename
+  t.is(getBasename('document.txt'), 'document');
+  t.is(getBasename('document.test.md'), 'document.test');
+});
+
+test('test getExtension function', (t) => {
+  // it should get the extension
+  t.is(getExtension('document.txt'), 'txt');
+  t.is(getExtension('document.test.txt'), 'txt');
+  t.is(getExtension('.txt'), '');
+  t.is(getExtension('qwe'), '');
+  t.is(getExtension(''), '');
 });
